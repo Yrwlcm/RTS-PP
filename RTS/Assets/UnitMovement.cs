d00e96ef03b27b68f1db8aa37b4c652cee1d3db2
Kholstinin Yegor <99079920+Yrwlcm@users.noreply.github.com>
2024-03-08 20:31:46 +0500

    Basic unit selection

--- RTS/Assets/UnitMovement.cs
+++ RTS/Assets/UnitMovement.cs
@@ -9,25 +9,22 @@ public class UnitMovement : MonoBehaviour
     private Camera mainCamera;
     private NavMeshAgent navAgent;
     public LayerMask ground;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         mainCamera = Camera.main;
         navAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        if (Input.GetMouseButtonDown((int)MouseButton.Right))
-        {
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (!Input.GetMouseButtonDown((int)MouseButton.Right)) return;
+        
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, ground))
-            {
-                navAgent.SetDestination(hit.point);
-            }
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, ground))
+        {
+            navAgent.SetDestination(hit.point);
         }
     }
 }
---
+++ RTS/Assets/UnitSelectionManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// Singleton класс, но из-за специфики юнити приходится делать фокусы с instanc-ами
+public class UnitSelectionManager : MonoBehaviour
+{
+    public static UnitSelectionManager Instance { get; private set; }
+
+    public readonly HashSet<GameObject> allUnits = new();
+    public readonly HashSet<GameObject> selectedUnits = new();
+
+    public LayerMask clickable;
+    public LayerMask ground;
+    public GameObject groundMarker;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        if (Instance is not null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown((int)MouseButton.Left)) return;
+
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            DeselectAll();
+        }
+
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, clickable))
+        {
+            ClickSelect(hit.collider.gameObject);
+        }
+    }
+
+    private void ClickSelect(GameObject unit)
+    {
+        var isSelected = selectedUnits.Add(unit);
+        if (!isSelected)
+            selectedUnits.Remove(unit);
+        
+        //Debug.Log($"{(isSelected ? "Selected" : "Deselected")} a unit", unit);
+        EnableUnitMovement(unit, isSelected);
+    }
+
+    private void EnableUnitMovement(GameObject unit, bool shouldMove)
+    {
+        unit.GetComponent<UnitMovement>().enabled = shouldMove;
+    }
+
+    private void DeselectAll()
+    {
+        foreach (var selectedUnit in selectedUnits)
+        {
+            EnableUnitMovement(selectedUnit, false);
+        }
+
+        selectedUnits.Clear();
+    }
+}
