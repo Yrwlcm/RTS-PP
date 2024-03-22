885b544dc60e447911ad35949a75061e87207eb8
Kholstinin Yegor <99079920+Yrwlcm@users.noreply.github.com>
2024-03-22 20:26:05 +0500

    Implemented camera panning

--- RTS/Assets/Scipts/CameraSystem.cs
+++ RTS/Assets/Scipts/CameraSystem.cs
@@ -1,85 +1,104 @@
+using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class CameraSystem : MonoBehaviour
 {
     [SerializeField] private int moveSpeed = 35;
     [SerializeField] private int rotateSpeed = 100;
     [SerializeField] private int edgeScrollingBorder = 20;
+    [SerializeField] Camera mainCamera;
 
     private bool dragPanMoveActive;
     private Vector2 lastMousePosition;
+    private Vector3 dragOrigin;
 
     void Update()
     {
         var inputDirection = new Vector3();
 
+        inputDirection = PanCamera();
+
         if (!dragPanMoveActive)
         {
-            inputDirection = CheckInputDirection();
-            inputDirection = CheckEdgeScrollDirection();
+            if (!CheckEdgeScrollDirection(out inputDirection))
+                CheckInputDirection(out inputDirection);
+
+            var moveDirection = transform.forward * inputDirection.z + transform.right * inputDirection.x;
+
+            inputDirection = moveDirection.normalized * moveSpeed * Time.deltaTime;   
         }
-        else
-        {
-            inputDirection = CheckClickDragDirection();
-        }
 
-        var moveDirection = transform.forward * inputDirection.z + transform.right * inputDirection.x;
+        transform.position += inputDirection;
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-        var rotateDirection = CheckRotateDirection();
+        CheckRotateDirection(out var rotateDirection);
 
         transform.eulerAngles += new Vector3(0, rotateDirection * rotateSpeed * Time.deltaTime, 0);
     }
 
-    private Vector3 CheckClickDragDirection()
+    private bool CheckRotateDirection(out int rotateDirection)
     {
-        if (Input.GetMouseButtonDown(2))
+        var localRotation = 0;
+
+        if (Input.GetKey(KeyCode.Q)) localRotation = 1;
+        if (Input.GetKey(KeyCode.E)) localRotation = -1;
+
+        rotateDirection = localRotation;
+        return localRotation != 0;
+    }
+
+    private Vector3 PanCamera()
+    {
+        if (Input.GetMouseButtonDown((int)MouseButton.Middle) && !dragPanMoveActive)
         {
-            lastMousePosition = Input.mousePosition;
+            dragOrigin = ScreenPositionToGroundRaycast(Input.mousePosition);
             dragPanMoveActive = true;
         }
 
-        var inputDirection = new Vector3();
-        var mouseMoveDelta = (Vector2)Input.mousePosition - lastMousePosition;
-
-        inputDirection.x = mouseMoveDelta.x;
-        inputDirection.z = mouseMoveDelta.y;
-
-        lastMousePosition = Input.mousePosition;
-
-        if (Input.GetMouseButtonDown(2))
+        if (!Input.GetMouseButton((int)MouseButton.Middle))
+        {
             dragPanMoveActive = false;
+            return new Vector3();
+        }
 
-        return new Vector3(-mouseMoveDelta.y, mouseMoveDelta.x, 0);
+        var difference = dragOrigin - ScreenPositionToGroundRaycast(Input.mousePosition);
+        difference.y = 0;
+        print("origin " + dragOrigin + " newPosition " + ScreenPositionToGroundRaycast(Input.mousePosition) +
+              " =difference" + difference);
+        return difference;
     }
 
-    private int CheckRotateDirection()
+    private bool CheckInputDirection(out Vector3 moveDirection)
     {
-        var rotateDirection = 0;
-        if (Input.GetKey(KeyCode.Q)) rotateDirection = 1;
-        if (Input.GetKey(KeyCode.E)) rotateDirection = -1;
-        return rotateDirection;
+        var localDirection = new Vector3();
+
+        if (Input.GetKey(KeyCode.UpArrow)) localDirection.z = 1;
+        if (Input.GetKey(KeyCode.DownArrow)) localDirection.z = -1;
+        if (Input.GetKey(KeyCode.LeftArrow)) localDirection.x = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) localDirection.x = 1;
+
+        moveDirection = localDirection;
+        return localDirection.magnitude > 0;
     }
 
-    private Vector3 CheckInputDirection()
+    private bool CheckEdgeScrollDirection(out Vector3 moveDirection)
     {
-        var inputDirection = new Vector3();
-        if (Input.GetKey(KeyCode.UpArrow)) inputDirection.z = 1;
-        if (Input.GetKey(KeyCode.DownArrow)) inputDirection.z = -1;
-        if (Input.GetKey(KeyCode.LeftArrow)) inputDirection.x = -1;
-        if (Input.GetKey(KeyCode.RightArrow)) inputDirection.x = 1;
-        return inputDirection;
-    }
+        var localDirection = new Vector3();
+        if (Input.mousePosition.x < edgeScrollingBorder) localDirection.x = -1;
+        if (Input.mousePosition.y < edgeScrollingBorder) localDirection.z = -1;
+        if (Input.mousePosition.x > Screen.width - edgeScrollingBorder) localDirection.x = +1;
+        if (Input.mousePosition.y > Screen.height - edgeScrollingBorder) localDirection.z = +1;
 
-    private Vector3 CheckEdgeScrollDirection()
+        moveDirection = localDirection;
+        return localDirection.magnitude > 0;
+    }
+    
+    //ScreenToWorldPoint у mainCamera не работает, почему - не знаю
+    //Загуглить не получилось, при этом все остальные функции которые конвертируют ворлд в скрин и наоборот работают
+    //Поэтому пришлось писать этот метод
+    private Vector3 ScreenPositionToGroundRaycast(Vector3 screenPosition)
     {
-        var inputDirection = new Vector3();
-        if (Input.mousePosition.x < edgeScrollingBorder) inputDirection.x = -1;
-        if (Input.mousePosition.y < edgeScrollingBorder) inputDirection.z = -1;
-        if (Input.mousePosition.x > Screen.width - edgeScrollingBorder) inputDirection.x = +1;
-        if (Input.mousePosition.y > Screen.height - edgeScrollingBorder) inputDirection.z = +1;
-        return inputDirection;
+        Physics.Raycast(mainCamera.ScreenPointToRay(screenPosition), out var hit, 100_000, LayerMask.GetMask("Ground"));
+
+        return hit.point;
     }
 }
